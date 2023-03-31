c1c3f14b131430e86dc8089c1f44d688d4e916ff
unknown <nicolasramirez22498@gmail.com>
2023-03-31 15:30:47 -0300

    funciones de los botones y operaciones

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// Función que muestra el valor en el input
+function mostrar(valor){
+	var input = document.getElementById('resultado');
+	input.value += valor;
+}
+
+// Función que realiza la operación
+function operacion(valor){
+	var input = document.getElementById('resultado');
+	input.value += valor;
+}
+
+// Función que limpia el input
+function limpiar(){
+	var input = document.getElementById('resultado');
+	input.value = "";
+}
+
+// Función que calcula el resultado
+function calcular(){
+	var input = document.getElementById('resultado');
+	var resultado = eval(input.value);
+	input.value = resultado;
+}
 
