2e8580d6fc98b4de663382c1521e5e3c26144449
unknown <nicolasramirez22498@gmail.com>
2023-03-31 23:17:26 -0300

    modificacion style botones

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Función que muestra el valor en el input
 function mostrar(valor){
-	var input = document.getElementById('resultado');
+	let input = document.getElementById('resultado');
 	input.value += valor;
 }
 
 // Función que realiza la operación
 function operacion(valor){
-	var input = document.getElementById('resultado');
+	let input = document.getElementById('resultado');
 	input.value += valor;
 }
 
 // Función que limpia el input
 function limpiar(){
-	var input = document.getElementById('resultado');
+	const input = document.getElementById('resultado');
 	input.value = "";
 }
 
 // Función que calcula el resultado
 function calcular(){
-	var input = document.getElementById('resultado');
-	var resultado = eval(input.value);
+	let input = document.getElementById('resultado');
+	let resultado = eval(input.value);
 	input.value = resultado;
 }
 
